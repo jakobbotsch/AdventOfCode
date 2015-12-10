a5a6c3db2ec969741b6f341773c2403478e241e2
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2015-12-10 13:29:00 +0100

    Day 10 solutions

---
+++ AdventOfCode/Day10.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace AdventOfCode
+{
+	internal static class Day10
+	{
+		public static int Part1(string input)
+		{
+			for (int i = 0; i < 40; i++)
+				input = Next(input);
+
+			return input.Length;
+		}
+
+		private static string Next(string cur)
+		{
+			StringBuilder sb = new StringBuilder();
+			int curPos = 0;
+			while (curPos < cur.Length)
+			{
+				char scan = cur[curPos];
+				int num;
+				for (num = curPos; num < cur.Length && cur[num] == scan; num++)
+				{
+				}
+
+				sb.Append(num - curPos);
+				sb.Append(scan);
+				curPos = num;
+			}
+
+			return sb.ToString();
+		}
+		public static int Part2(string input)
+		{
+			for (int i = 0; i < 50; i++)
+				input = Next(input);
+
+			return input.Length;
+		}
+	}
+}
