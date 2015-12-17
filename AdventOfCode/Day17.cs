7e25b2296fca734fd88b98925b5a31f150fff4fa
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2015-12-17 11:21:14 +0100

    Day 17 solutions

---
+++ AdventOfCode/Day17.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Combinatorics.Collections;
+
+namespace AdventOfCode
+{
+	internal static class Day17
+	{
+		public static int Part1(string[] input)
+		{
+			int count = 0;
+			for (int i = 1; i <= input.Length; i++)
+			{
+				count += new Combinations<string>(input, i).Count(comb => comb.Sum(int.Parse) == 150);
+			}
+
+			return count;
+		}
+
+		public static int Part2(string[] input)
+		{
+			for (int i = 1; i <= input.Length; i++)
+			{
+				int count = new Combinations<string>(input, i).Count(comb => comb.Sum(int.Parse) == 150);
+				if (count > 0)
+					return count;
+			}
+
+			return -1;
+		}
+	}
+}
