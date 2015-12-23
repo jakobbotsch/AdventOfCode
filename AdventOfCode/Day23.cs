67107081b2f5f4ce11b9d919ee1cc0c6691ef16d
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2015-12-23 16:02:02 +0100

    Day 23 solutions

---
+++ AdventOfCode/Day23.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Properties;
+
+namespace AdventOfCode
+{
+	internal static class Day23
+	{
+		public static uint Part1(string[] instructions)
+		{
+			var regs = new DefaultDictionary<string, uint>();
+			Simulate(instructions, regs);
+			return regs["b"];
+		}
+
+		public static uint Part2(string[] instructions)
+		{
+			var regs = new DefaultDictionary<string, uint> {["a"] = 1};
+			Simulate(instructions, regs);
+			return regs["b"];
+		}
+
+		private static void Simulate(string[] instructions, DefaultDictionary<string, uint> regs)
+		{
+			int ip = 0;
+			while (ip >= 0 && ip < instructions.Length)
+			{
+				string ins = instructions[ip];
+				if (ins.StartsWith("hlf") || ins.StartsWith("tpl") || ins.StartsWith("inc"))
+				{
+					string reg = ins[4].ToString();
+					switch (ins.Substring(0, 3))
+					{
+						case "hlf":
+							regs[reg] /= 2;
+							break;
+						case "tpl":
+							regs[reg] *= 3;
+							break;
+						case "inc":
+							regs[reg]++;
+							break;
+					}
+
+					ip++;
+					continue;
+				}
+
+				if (ins.StartsWith("jmp"))
+				{
+					ip += int.Parse(ins.Substring(4));
+					continue;
+				}
+
+				if (ins.StartsWith("jie") || ins.StartsWith("jio"))
+				{
+					var reg = ins[4].ToString();
+					int offset = int.Parse(ins.Substring(6));
+					if (ins.StartsWith("jie") && regs[reg]%2 == 0)
+						ip += offset;
+					else if (ins.StartsWith("jio") && regs[reg] == 1)
+						ip += offset;
+					else
+						ip++;
+				}
+			}
+		}
+	}
+}
---
+++ AdventOfCode/Properties/DefaultDictionary.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Properties
+{
+	public class DefaultDictionary<TKey, TValue> : IDictionary<TKey, TValue>,
+	                                               IReadOnlyDictionary<TKey, TValue>
+	{
+		private readonly Dictionary<TKey, TValue> _dict;
+		private readonly TValue _defaultValue;
+
+		public DefaultDictionary()
+		{
+			_dict = new Dictionary<TKey, TValue>();
+		}
+
+		public DefaultDictionary(TValue defaultValue)
+		{
+			_dict = new Dictionary<TKey, TValue>();
+			_defaultValue = defaultValue;
+		}
+
+		public DefaultDictionary(TValue defaultValue, IEqualityComparer<TKey> comparer)
+		{
+			_dict = new Dictionary<TKey, TValue>(comparer);
+			_defaultValue = defaultValue;
+		}
+
+		public ICollection<TKey> Keys => _dict.Keys;
+
+		public ICollection<TValue> Values => _dict.Values;
+
+		public int Count => _dict.Count;
+
+		public TValue this[TKey key]
+		{
+			get
+			{
+				TValue val;
+				TryGetValue(key, out val);
+				return val;
+			}
+			set { _dict[key] = value; }
+		}
+
+		public bool TryGetValue(TKey key, out TValue value)
+		{
+			if (!_dict.TryGetValue(key, out value))
+			{
+				value = _defaultValue;
+				return false;
+			}
+
+			return true;
+		}
+
+		public void Add(TKey key, TValue value)
+		{
+			_dict.Add(key, value);
+		}
+
+		public bool Remove(TKey key)
+		{
+			return _dict.Remove(key);
+		}
+
+		public bool ContainsKey(TKey key)
+		{
+			return _dict.ContainsKey(key);
+		}
+
+		public void Clear()
+		{
+			_dict.Clear();
+		}
+
+		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+		{
+			return _dict.GetEnumerator();
+		}
+
+		IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => _dict.Values;
+
+		IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => _dict.Keys;
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable)_dict).GetEnumerator();
+		}
+
+		void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
+		{
+			((ICollection<KeyValuePair<TKey, TValue>>)_dict).Add(item);
+		}
+
+		bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item)
+		{
+			return ((ICollection<KeyValuePair<TKey, TValue>>)_dict).Contains(item);
+		}
+
+		void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array,
+		                                                    int arrayIndex)
+		{
+			((ICollection<KeyValuePair<TKey, TValue>>)_dict).CopyTo(array, arrayIndex);
+		}
+
+		bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
+		{
+			return ((ICollection<KeyValuePair<TKey, TValue>>)_dict).Remove(item);
+		}
+
+		bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly
+			=> ((ICollection<KeyValuePair<TKey, TValue>>)_dict).IsReadOnly;
+	}
+}
