57f162392eafe0fbd068d89bf1a05602b806d896
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2015-12-25 13:22:22 +0100

    Day 25 solutions

---
+++ AdventOfCode/Day25.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode
+{
+	internal static class Day25
+	{
+		public static long Part1()
+		{
+			int row = 1, column = 1;
+			long result = 20151125;
+
+			while (row != 3010 || column != 3019)
+			{
+				result *= 252533;
+				result %= 33554393;
+
+				column++;
+				row--;
+
+				if (row <= 0)
+				{
+					row = column;
+					column = 1;
+				}
+			}
+
+			return result;
+		}
+	}
+}
