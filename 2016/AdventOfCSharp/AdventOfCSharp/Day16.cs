a3a0b5bbea2a6d6f401e55d40e89c54fad1638e9
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2016-12-16 11:45:17 +0100

    Day 16

---
+++ 2016/AdventOfCSharp/AdventOfCSharp/Day16.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCSharp
+{
+    internal static class Day16
+    {
+        internal static void Solve(string input)
+        {
+            const int length = 35651584;
+            string a = input;
+            while (a.Length < length)
+            {
+                string b = a;
+                a += "0" + new string(b.Reverse().Select(c => c == '0' ? '1' : '0').ToArray());
+            }
+
+            a = a.Remove(length);
+
+            while (a.Length % 2 == 0)
+            {
+                StringBuilder newA = new StringBuilder();
+                for (int i = 0; i < a.Length; i += 2)
+                {
+                    if (a[i] == a[i + 1])
+                        newA.Append("1");
+                    else
+                        newA.Append("0");
+                }
+
+                a = newA.ToString();
+            }
+
+            Console.WriteLine("Checksum: {0}", a);
+        }
+    }
+}
--- 2016/AdventOfCSharp/AdventOfCSharp/Program.cs
+++ 2016/AdventOfCSharp/AdventOfCSharp/Program.cs
@@ -6,12 +6,7 @@ namespace AdventOfCSharp
     {
         private static void Main(string[] args)
         {
-            Day15.Solve(@"Disc #1 has 13 positions; at time=0, it is at position 11.
-Disc #2 has 5 positions; at time=0, it is at position 0.
-Disc #3 has 17 positions; at time=0, it is at position 11.
-Disc #4 has 3 positions; at time=0, it is at position 0.
-Disc #5 has 7 positions; at time=0, it is at position 2.
-Disc #6 has 19 positions; at time=0, it is at position 17.");
+            Day16.Solve("10111011111001111");
             Console.ReadLine();
         }
     }
