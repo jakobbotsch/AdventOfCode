77dbec9c092287d0498081b9572d47c6b11bc5e8
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2017-12-04 19:21:55 +0100

    Day 4

---
+++ 2017/AdventOfCode/Day4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    internal static class Day4
+    {
+        public static void Solve1(string input)
+        {
+            Console.WriteLine(Util.GetLines(input).Count(s => s.Split().All(new HashSet<string>().Add)));
+        }
+
+        public static void Solve2(string input)
+        {
+            Console.WriteLine(Util.GetLines(input).Count(s =>
+            {
+                var hs = new HashSet<string>();
+                return s.Split().All(w => hs.Add(string.Concat(w.OrderBy(c => c))));
+            }));
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    internal static class Util
+    {
+        public static string[] GetLines(string input)
+        {
+            return input.Replace(Environment.NewLine, "\n").Split('\n');
+        }
+    }
+}
