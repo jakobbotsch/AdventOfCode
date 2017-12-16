90e12085b6a7c8357fa19593084a8542ca2f1850
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2017-12-16 10:41:43 +0100

    Day 16

---
+++ 2017/AdventOfCode/Day16.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    internal static class Day16
+    {
+        public static void Solve(string input)
+        {
+            string curProg = "abcdefghijklmnop";
+            string[] insts = input.Split(',');
+            Dictionary<string, string> cache = new Dictionary<string, string>();
+
+            for (int times = 0; times < 1000000000; times++)
+            {
+                if (times % 100000 == 0)
+                    Console.Title = $"{times}";
+
+                if (cache.TryGetValue(curProg, out string newProg))
+                {
+                    curProg = newProg;
+                    continue;
+                }
+
+                char[] progs = curProg.ToCharArray();
+                foreach (string inst in insts)
+                {
+                    if (inst[0] == 's')
+                    {
+                        for (int i = 0, end = int.Parse(inst.Substring(1)); i < end; i++)
+                        {
+                            char newFront = progs.Last();
+                            for (int j = progs.Length - 1; j > 0; j--)
+                                progs[j] = progs[j - 1];
+
+                            progs[0] = newFront;
+                        }
+                        continue;
+                    }
+
+                    if (inst[0] == 'x')
+                    {
+                        int slash = inst.IndexOf('/');
+                        int a = int.Parse(inst.Substring(1, slash - 1));
+                        int b = int.Parse(inst.Substring(slash + 1));
+                        char c = progs[a];
+                        progs[a] = progs[b];
+                        progs[b] = c;
+                        continue;
+                    }
+
+                    if (inst[0] == 'p')
+                    {
+                        char c1 = inst[1];
+                        char c2 = inst[3];
+                        int a = -1, b = -1;
+                        for (int i = 0; i < progs.Length; i++)
+                        {
+                            if (progs[i] == c1)
+                                a = i;
+                            if (progs[i] == c2)
+                                b = i;
+                        }
+
+                        char c = progs[a];
+                        progs[a] = progs[b];
+                        progs[b] = c;
+                    }
+                }
+
+                newProg = new string(progs);
+
+                if (times == 0)
+                    Console.WriteLine(newProg);
+
+                cache.Add(curProg, newProg);
+                curProg = newProg;
+            }
+
+            Console.WriteLine(curProg);
+        }
+    }
+}
